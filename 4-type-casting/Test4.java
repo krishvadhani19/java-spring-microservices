e05e6b186051694cd45e3202fce90cd9b6784b33
Krish Vadhani <vadhani.k@northeastern.edu>
2024-09-15 14:00:20 -0400

    Type casting

---
+++ 4-type-casting/Test4.java
@@ -0,0 +1,14 @@
+
+public class Test4 {
+
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+
+        /*
+        This is called type conversion or type promotion
+        Since maximum value stored in byte 127 we multiplied 2 bytes and logged the result
+         */
+        System.out.println(a * b);
+    }
+}
