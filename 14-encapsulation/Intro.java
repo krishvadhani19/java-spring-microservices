9afaa118b002ea3182e49c6356256bf4b4e951cf
Krish Vadhani <krishvadhani7@gmail.com>
2024-09-17 16:28:03 -0400

    Encapsulation

--- 14-encapsulation/Intro.java
+++ 14-encapsulation/Intro.java
@@ -1,6 +1,45 @@
 
-public class Intro {
+class Human {
+
+    private int age;
+    private String name;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setAge(int newAge) {
+        this.age = newAge;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void show() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Age: " + this.age);
+    }
+}
+
+class Intro {
+
     public static void main(String[] args) {
-        
+        Human h1 = new Human("Krish", 23);
+        h1.show();
+
+        h1.setAge(82);
+        h1.setName("Krish Hero");
+
+        h1.show();
     }
 }
