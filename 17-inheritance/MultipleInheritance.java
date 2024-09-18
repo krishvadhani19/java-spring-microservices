726b57af92ea231930bb8e05a34c0ff7ab505184
Krish Vadhani <krishvadhani7@gmail.com>
2024-09-17 20:13:05 -0400

    Multiple Inheritance

---
+++ 17-inheritance/MultipleInheritance.java
@@ -0,0 +1,27 @@
+
+class A {
+
+}
+
+class B extends A {
+
+}
+
+class C extends B {
+
+}
+
+public class MultipleInheritance {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+/**
+ * Multiple Inheritance is not possible in Java
+ * i.e A child can have only 1 parent and not 2
+ * Simple words: Lets say the child parents method and both have same name method 
+ * Child wont be able to make a choice
+ */
