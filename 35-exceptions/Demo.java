60abe25c4c7b1550cd503b4b66e51cced950cd86
Krish Vadhani <krishvadhani7@gmail.com>
2024-09-23 22:58:54 -0400

    Exceptions done

--- 35-exceptions/Demo.java
+++ 35-exceptions/Demo.java
@@ -1,16 +1,28 @@
 
+class CustomException extends Exception {
+
+    public CustomException(String exceptionMessage) {
+        super(exceptionMessage);
+    }
+}
+
 public class Demo {
 
     public static void main(String[] args) {
-        int i = 2;
+        int i = 20;
         int j = 0;
 
         try {
             j = 18 / i;
+            if (j == 0) {
+                throw new CustomException("I dont want the ans as zero");
+            }
         } catch (ArithmeticException e) {
-            System.out.println("Cannot divide by zero!");
+            System.out.println("Cannot divide by zero!" + e);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Stay in yur limit");
+        } catch (CustomException e) {
+            System.out.println("Custom Exception");
         } catch (Exception e) {
             System.out.println("something went wrong");
         }
